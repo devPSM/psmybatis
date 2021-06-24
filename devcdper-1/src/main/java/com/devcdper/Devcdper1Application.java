deaa1effad80b204f0064bbe46772530d4c0863b
psm <psm@localhost>
2021-06-24 09:44:02 +0900

    [ 03team ] teamProject v1 file		: 경력개발계획 중 계획관련파일 detail	: - 컨트롤러 통합 		  - left, header통합 		  - mainCdp.html (나의계획한눈에보기) 		  - plan,lastn 부분만 경로잡음. 		  - 통합계획, 한눈에보기는 어느정도 화면작성완료 		  - 내꺼만 푸쉬해놓은 상태 다른 팀원들꺼 pull안함

---
+++ devcdper-1/src/main/java/com/devcdper/Devcdper1Application.java
@@ -0,0 +1,13 @@
+package com.devcdper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Devcdper1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Devcdper1Application.class, args);
+	}
+
+}
